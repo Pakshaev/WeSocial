9416631ce5c23bbaa2c45209637d4d29318ec662
Gleb Pakshaev <pakshaev2003@gmail.com>
2023-02-28 13:31:26 +0400

    added user repo added post owner display bug fixed

---
+++ src/main/java/com/example/WeSocial/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.WeSocial.repos;
+
+import com.example.WeSocial.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
